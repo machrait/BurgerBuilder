63f9237725e5a9ad0cb3a62d82d5ddf8c46ae9fa
machrait <machrait@spacebyte.com>
2020-04-28 18:36:19 +0000

    128 wip

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,7 +5,7 @@ import classes from './Layout.module.css'
 
 const layout = (props) => (
 	<Aux>
-		<div>Toolbar, SideDrawer, Baackdrop</div>
+		<div>Toolbar, SideDrawer, Backdrop</div>
 		<main className={classes.Content}>
 			{props.children}
 		</main>
