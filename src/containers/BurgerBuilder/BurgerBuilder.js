b99a4a77975d7de7d828195033e472ce017b5776
machrait <machrait@spacebyte.com>
2020-05-02 14:26:21 +0000

    135. Displaying and updating the Burger Price

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -71,9 +71,11 @@ class BurgerBuilder extends Component
 			<Aux>
 				<Burger ingredients={this.state.ingredients}/>
 				<BuildControls
-				ingredientAdded = {this.addIngredientHandler}
-				ingredientRemoved = {this.removeIngredientHandler}
-				disabled= {disabledInfo}/>
+					ingredientAdded = {this.addIngredientHandler}
+					ingredientRemoved = {this.removeIngredientHandler}
+					disabled = {disabledInfo}
+					price = {this.state.totalPrice}
+				/>
 			</Aux>
 		);
 	}
