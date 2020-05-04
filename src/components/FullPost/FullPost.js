7cdfe701963b1d1cf4e44b29c0b90c4c27838a34
machrait <machrait@spacebyte.com>
2020-05-04 20:37:00 +0000

    173. Setting a Default Global Configuration for Axios

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -15,7 +15,7 @@ class FullPost extends Component {
         {
 			if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id))
 			{
-				axios.get('https://jsonplaceholder.typicode.com/posts/'+ this.props.id).then(response =>
+				axios.get('/posts/'+ this.props.id).then(response =>
 				{
 					this.setState({loadedPost: response.data});
 				});
@@ -26,7 +26,7 @@ class FullPost extends Component {
 
 	deletePostHandler = () =>
 	{
-		axios.delete('https://jsonplaceholder.typicode.com/posts/'+ this.props.id).then(response =>
+		axios.delete('/posts/'+ this.props.id).then(response =>
 		{
 			console.log('response',response);
 		});
