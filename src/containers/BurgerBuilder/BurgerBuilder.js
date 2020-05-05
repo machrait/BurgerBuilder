c061fd57efad86b6e3d60a62bc81512f05255f98
machrait <machrait@spacebyte.com>
2020-05-05 22:20:53 +0000

    181. Sending a POST Request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Burger from '../../components/Burger/Burger';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
 	salad: 0.5,
@@ -88,7 +89,27 @@ class BurgerBuilder extends Component
 
 	purchaseContinueHandler = () =>
 	{
-		alert('You continue!');
+		//alert('You continue!');
+		const order =
+		{
+			ingredients: this.state.ingredients,
+			price: this.state.totalPrice,
+			customer: 
+			{
+				name: 'Meryem Achrait',
+				address: 
+				{
+					street: 'Lot Arreda',
+					zipcode: '454546',
+					country: 'Morocco'
+				},
+				email: 'test@mail.com'
+			},
+			deliveryMethod: 'fastest'
+		};
+		axios.post('/orders.json',order)
+		.then(response =>	console.log(response))
+		.catch(error =>		console.log(error));
 	}
 
 	render()
