363395a009ecec00463aa4ff40d4ee4454a1f066
machrait <machrait@spacebyte.com>
2020-05-07 21:42:42 +0000

    206. Navigating Programmatically

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
-import { Link } from 'react-router-dom';
 
 import Post from '../../../components/Post/Post';
 import './Posts.css';
@@ -32,7 +31,7 @@ class Posts extends Component
 
 	postSelectedHandler = (id) =>
 	{
-		this.setState({selectedPostId: id});
+		this.props.history.push({pathname: '/' + id});
 	}
 
 
@@ -44,14 +43,14 @@ class Posts extends Component
 		posts = this.state.posts.map(post=>
 		{
 			return	(
-				<Link 
-					to={'/'+post.id}
-					key={post.id} >
+				//<Link 
 					<Post 
+						key={post.id}
 						title={post.title} 
 						author={post.author}
 						clicked={() => this.postSelectedHandler(post.id)}/>
-				</Link>);
+				//</Link>
+				);
 		});
 	}
     return (
