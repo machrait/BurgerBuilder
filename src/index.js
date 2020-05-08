7abbf05c616ab3cf827ead8016ba7dd8675ecb71
machrait <machrait@spacebyte.com>
2020-05-08 15:58:01 +0000

    suspense project setup

--- src/index.js
+++ src/index.js
@@ -1,31 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-import axios from 'axios';
+ReactDOM.render(<App />, document.getElementById('root'));
 
-axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
-axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
-axios.defaults.headers.post['Content-Type'] = 'application/json';
-
-
-axios.interceptors.request.use(request=>{
-	console.log('request',request);
-	return request;
-}, error =>{
-	console.log('error',error);
-	return Promise.reject(error);
-});
-
-axios.interceptors.response.use(response=>{
-	console.log('response',response);
-	return response;
-}, error =>{
-	console.log('error',error);
-	return Promise.reject(error);
-});
-
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
-registerServiceWorker();
