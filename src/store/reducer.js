89dcca724d6d27c1432eb30c1634504e91a73e25
machrait <machrait@spacebyte.com>
2020-05-09 23:03:56 +0000

    260. Dispatching Actions from within the Component

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -5,6 +5,12 @@ const initialState =
 
 const reducer = (state = initialState, action) =>
 {
+	if(action.type === 'INCREMENT')
+	{
+		return{
+			counter: state.counter + 1
+		}
+	}
 	return state;
 };
 
