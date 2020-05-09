a02f08bbcf1a4eb5f206b393f99e5623daa836b2
machrait <machrait@spacebyte.com>
2020-05-09 00:07:46 +0000

    228. Adding an Orders Page

---
+++ src/containers/Orders/Orders.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Order from '../../components/Order/Order';
+
+class Orders extends Component 
+{
+	state = {
+		
+	}
+
+	componentWillMount()
+	{
+		
+	}
+	
+
+	render() 
+	{
+		return (
+			<div>
+				<Order/>
+				<Order/>
+			</div>
+		);
+	}
+}
+
+export default Orders;
