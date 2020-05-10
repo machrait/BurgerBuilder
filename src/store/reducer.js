8ca43c8ce266648e75e75052791a71012d18a335
machrait <machrait@spacebyte.com>
2020-05-10 00:13:17 +0000

    266. Outsourcing Action Types

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,3 +1,5 @@
+import * as actionTypes from './actions';
+
 const initialState = 
 {
 	counter: 0,
@@ -8,32 +10,32 @@ const reducer = (state = initialState, action) =>
 {
 	switch (action.type)
 	{
-		case 'INCREMENT':
+		case actionTypes.INCREMENT:
 			return{
 				...state,
 				counter: state.counter + 1
 			}
-		case 'DECREMENT':
+		case actionTypes.DECREMENT:
 			return{
 				...state,
 				counter: state.counter - 1
 			}
-		case 'ADD':
+		case actionTypes.ADD:
 			return{
 				...state,
 				counter: state.counter + 5
 			}
-		case 'SUBTRACT':
+		case actionTypes.SUBTRACT:
 			return{
 				...state,
 				counter: state.counter - 5
 			}
-		case 'STORE_RESULT':
+		case actionTypes.STORE_RESULT:
 			return{
 				...state,
 				results: state.results.concat({id: new Date(), value: state.counter})
 			}
-		case 'DELETE_RESULT':
+		case actionTypes.DELETE_RESULT:
 			const updatedArray = state.results.filter(result => result.id!== action.resultElId);
 			return{
 				...state,
