08197bc30632aed8f1e9c91cb427ffed27aa26cf
machrait <machrait@spacebyte.com>
2020-05-10 13:55:19 +0000

    270. Combining Local UI State and Redux

--- src/components/AddPerson/AddPerson.js
+++ src/components/AddPerson/AddPerson.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './AddPerson.css';
 
-const addPerson = (props) => (
-    <div className="AddPerson">
-        <button onClick={props.personAdded}>Add Person</button>
-    </div>
-);
-
-export default addPerson;
+class AddPerson extends Component 
+{
+	state = 
+	{
+		name: '',
+		age: 0
+	}
+	nameChangedhandler = (event) =>
+	{
+		this.setState({name: event.target.value});
+	}
+	ageChangedhandler = (event) =>
+	{
+		this.setState({age: event.target.value});
+	}
+	render() {
+	 	return (
+			<div className="AddPerson">
+				<input 
+					onChange={this.nameChangedhandler}
+					value={this.state.name}
+					type="text" 
+					placeholder="Enter your name"/>
+				<input 
+					onChange={this.ageChangedhandler}
+					value={this.state.age}
+					type="number" 
+					placeholder="Enter your age"/>
+				<button 
+					onClick={() => this.props.personAdded(this.state.name, this.state.age)}>Add Person</button>
+			</div>);
+	}
+}
+export default AddPerson;
