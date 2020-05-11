42ab0cf3db651d0a811574cf4a885797bd666dd6
machrait <machrait@spacebyte.com>
2020-05-11 22:09:16 +0000

    307. Adding Order Actions

---
+++ src/store/actions/order.js
@@ -0,0 +1,35 @@
+import * as actionTypes from './actionsTypes';
+import axios from '../../axios-orders';
+
+
+export const purchaseBurgerSuccess = (id, orderData) =>
+{
+	return {
+		type: actionTypes.PURHCASE_BURGER_SUCCESS,
+		orderId: id,
+		orderData: orderData
+	}
+}
+
+export const purchaseBurgerFail = (error) =>
+{
+	return {
+		type: actionTypes.PURHCASE_BURGER_FAIL,
+		error: error
+	}
+}
+
+export const purchaseBurgerStart = (orderData) =>
+{
+	return dispatch =>
+	{
+		axios.post('/orders.json',orderData)
+		.then(response =>	{
+			dispatch(purchaseBurgerSuccess(response.data, orderData));
+		})
+		.catch(error =>	{
+			dispatch(purchaseBurgerFail(error));
+		});
+	};
+	
+}
