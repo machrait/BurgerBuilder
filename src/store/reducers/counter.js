81ed7a478601c47a7d610f7248f0d384480f3371
machrait <machrait@spacebyte.com>
2020-05-11 16:15:55 +0000

    294. A Leaner Switch Case Statement

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,5 +1,6 @@
 import * as actionTypes from '../actions/actionTypes';
 import { updateObject } from '../utility';
+
 const initialState = 
 {
 	counter: 0
@@ -9,16 +10,11 @@ const reducer = (state = initialState, action) =>
 {
 	switch (action.type)
 	{
-		case actionTypes.INCREMENT:
-			return updateObject(state, {counter: state.counter + 1});
-		case actionTypes.DECREMENT:
-			return updateObject(state, {counter: state.counter - 1});
-		case actionTypes.ADD:
-			return updateObject(state, {counter: state.counter + action.val});
-		case actionTypes.SUBTRACT:
-			return updateObject(state, {counter: state.counter - action.val});
-		default:
-			return state;
+		case actionTypes.INCREMENT: return updateObject(state, {counter: state.counter + 1});
+		case actionTypes.DECREMENT: return updateObject(state, {counter: state.counter - 1});
+		case actionTypes.ADD: return updateObject(state, {counter: state.counter + action.val});
+		case actionTypes.SUBTRACT: return updateObject(state, {counter: state.counter - action.val});
+		default: return state;
 	}
 };
 
