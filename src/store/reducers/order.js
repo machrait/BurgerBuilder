3dbfa110914a5af611460e246873becb6d69f40a
machrait <machrait@spacebyte.com>
2020-05-12 12:46:48 +0000

    318. Refactoring Reducers Continued

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -7,31 +7,34 @@ const initialeState = {
 	purchased: false
 };
 
+const purchaseSuccess = (state, action) =>
+{
+	const newOrder = updateObject(action.orderData, {id: action.orderId});
+	return updateObject(state, {
+		loading: false,
+		orders: state.orders.concat(newOrder),
+		purchased: true});
+};
+
+const fetchOrdersSuccess = (state, action) =>
+{
+	return updateObject(state, {
+		loading: false,
+		orders: action.orders});
+};
 
 const reducer = (state = initialeState, action) =>
 {
 	switch (action.type)
 	{
-		case actionTypes.PURHCASE_INIT:
-			return updateObject(state, {purchased: false});
-		case actionTypes.PURHCASE_BURGER_SUCCESS:
-			const newOrder = updateObject(action.orderData, {id: action.orderId});
-			return updateObject(state, {
-				loading: false,
-				orders: state.orders.concat(newOrder),
-				purchased: true});
+		case actionTypes.PURHCASE_INIT: return updateObject(state, {purchased: false});
+		case actionTypes.PURHCASE_BURGER_SUCCESS: return purchaseSuccess(state, action);
 		case actionTypes.PURHCASE_BURGER_FAIL:
-		case actionTypes.FETCH_ORDERS_FAIL:
-			return updateObject(state, {loading: false});
+		case actionTypes.FETCH_ORDERS_FAIL: return updateObject(state, {loading: false});
 		case actionTypes.PURCHASE_BURGER_START:
-		case actionTypes.FETCH_ORDERS_START:
-			return updateObject(state, {loading: true});
-		case actionTypes.FETCH_ORDERS_SUCCESS:
-			return updateObject(state, {
-				loading: false,
-				orders: action.orders});
-		default:
-			return state;
+		case actionTypes.FETCH_ORDERS_START: return updateObject(state, {loading: true});
+		case actionTypes.FETCH_ORDERS_SUCCESS: return fetchOrdersSuccess(state, action);
+		default: return state;
 	}
 }
 	
