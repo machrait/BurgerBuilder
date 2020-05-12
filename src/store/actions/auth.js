f4f8ce4a62e4033c52d4ecda6c18658591efbc4f
machrait <machrait@spacebyte.com>
2020-05-12 16:02:41 +0000

    326. Getting a Token from the Backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,5 @@
 import * as actionTypes from './actionsTypes';
-
+import axios from 'axios';
 
 export const authStart = () =>
 {
@@ -28,8 +28,21 @@ export const authFail = (error) =>
 export const auth = (email, password) =>
 {
 	return dispatch =>
-	{
+	{	
 		dispatch(authStart());
+		const authData = {
+			email: email,
+			password: password,
+			returnSecureToken: true
+		};
+		axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDHyCgtV0pAkcTkaVBJgEUMTLgnd6S3QOg',authData)
+		.then(response => 
+		{
+			dispatch(authSuccess(response.data));
+		}).catch(error => 
+		{
+			dispatch(authFail(error));
+		});
 	};
 	
 }
