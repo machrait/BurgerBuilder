887640bdad8925881dbaef9cac7269ea0b421e7c
machrait <achrait.meryem@gmail.com>
2020-05-13 13:16:59 +0000

    336. Persistent Auth State with localStorage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -8,7 +9,12 @@ import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 
+import * as actions from './store/actions';
 class App extends Component {
+	componentDidMount()
+	{
+		this.props.onAuthCheckState();
+	}
 	render() {
 		return (
 			<div>
@@ -26,4 +32,11 @@ class App extends Component {
 	}
 }
 
-export default App;
+const mapDispatchToProps = dispath => {
+	return {
+		onAuthCheckState: () => dispath(actions.authCheckState()),
+	}
+}
+
+ 
+export default connect(null, mapDispatchToProps)(App);
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -5,8 +5,8 @@ export const authStart = () =>
 {
 	return {
 		type: actionTypes.AUTH_START
-	}
-}
+	};
+};
 
 export const authSuccess = (idToken, userId) =>
 {
@@ -14,31 +14,34 @@ export const authSuccess = (idToken, userId) =>
 		type: actionTypes.AUTH_SUCCESS,
 		idToken: idToken,
 		userId: userId
-	}
-}
+	};
+};
 
 export const authFail = (error) =>
 {
 	return {
 		type: actionTypes.AUTH_FAIL,
 		error: error
-	}
-}
+	};
+};
 
 export const logout = () =>
 {
+	localStorage.removeItem('token');
+	localStorage.removeItem('userId');
+	localStorage.removeItem('expirationDate');
 	return {
 		type: actionTypes.AUTH_LOGOUT
-	}
-}
+	};
+};
 
 export const chechAuthTimeout = (expirationTime) =>
 {
 	return dispatch =>
 	{
 		setTimeout(() => { dispatch(logout())}, expirationTime * 1000);
-	}
-}
+	};
+};
 
 export const auth = (email, password, isSignup) =>
 {
@@ -58,6 +61,10 @@ export const auth = (email, password, isSignup) =>
 		axios.post(url,authData)
 		.then(response => 
 		{
+			const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+			localStorage.setItem('token', response.data.idToken);
+			localStorage.setItem('expirationDate', expirationDate);
+			localStorage.setItem('userId', response.data.localId);
 			dispatch(authSuccess(response.data.idToken,response.data.localId));
 			dispatch(chechAuthTimeout(response.data.expiresIn));
 
@@ -67,7 +74,7 @@ export const auth = (email, password, isSignup) =>
 		});
 	};
 	
-}
+};
 
 export const setAuthRedirectPath = (path) =>
 {
@@ -76,3 +83,29 @@ export const setAuthRedirectPath = (path) =>
 		path: path
 	};
 };
+
+export const authCheckState = () =>
+{
+	return dispatch =>
+	{
+		const token = localStorage.getItem('token');
+		if (!token)
+		{
+			dispatch(logout());
+		}
+		else
+		{
+			const expirationDate = new Date(localStorage.getItem('expirationDate'));
+			const userId = localStorage.getItem('userId');
+			if (expirationDate > new Date())
+			{
+				dispatch(authSuccess(token,userId));
+				dispatch(chechAuthTimeout(expirationDate.getSeconds() - new Date.getSeconds()));
+			}
+			else
+			{
+				dispatch(logout());
+			}
+		}
+	}
+}
