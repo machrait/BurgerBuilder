13dc7a1151176c15d97ac3dcee00e147ae7fce60
machrait <achrait.meryem@gmail.com>
2020-05-15 23:31:28 +0000

    393. Deploying our App

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import User from '../../components/User';
+import React from "react";
 
-const authIndexPage = () =>
-(
-	<div>
-		<h1>The Auth Index main Page</h1>
-		<User name="Mey" age={25}/>
-	</div>
+import User from "../../components/User";
+
+const authIndexPage = (props) => (
+  <div>
+    <h1>The Auth Index Page - {props.appName}</h1>
+    <User name="Max" age={28} />
+  </div>
 );
+
+authIndexPage.getInitialProps = context => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ appName: "Super App (Auth)" });
+    }, 1000);
+  });
+  return promise;
+};
 
 export default authIndexPage;
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,31 @@
-import React, { Component } from 'react';
-import Link from 'next/link';
-import Router from 'next/router';
+import React, { Component } from "react";
+import Link from "next/link";
+import Router from "next/router";
 
-class IndexPage extends Component
-{
-	static async getInitialProps(context)
-	{
-		console.log(context);
-		return {};
-	}
-	render () {
-		return (
-			<div>
-				<h1>The main Page</h1>
-				<p>Go to <Link href="/auth"><a>Auth</a></Link></p>
-				<button onClick={() => Router.push('/auth')}>Go to Auth</button>
-			</div>
-		);
-	}
+class IndexPage extends Component {
+  static getInitialProps(context) {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ appName: "Super App" });
+      }, 1000);
+    });
+    return promise;
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>The Main Page of {this.props.appName}</h1>
+        <p>
+          Go to{" "}
+          <Link href="/auth">
+            <a>Auth</a>
+          </Link>
+        </p>
+        <button onClick={() => Router.push("/auth")}>Go to Auth</button>
+      </div>
+    );
+  }
 }
+
 export default IndexPage;
