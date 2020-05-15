e083052596ac929604bb12c9867e38984ab7a150
machrait <achrait.meryem@gmail.com>
2020-05-15 23:25:49 +0000

    392. A Special Lifecycle Hook

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
 
-const indexPage = () =>
-(
-	<div>
-		<h1>The main Page</h1>
-		<p>Go to <Link href="/auth"><a>Auth</a></Link></p>
-		<button onClick={() => Router.push('/auth')}>Go to Auth</button>
-	</div>
-);
-
-export default indexPage;
+class IndexPage extends Component
+{
+	static async getInitialProps(context)
+	{
+		console.log(context);
+		return {};
+	}
+	render () {
+		return (
+			<div>
+				<h1>The main Page</h1>
+				<p>Go to <Link href="/auth"><a>Auth</a></Link></p>
+				<button onClick={() => Router.push('/auth')}>Go to Auth</button>
+			</div>
+		);
+	}
+}
+export default IndexPage;
