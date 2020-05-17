bfa22ec354d4f70d2394a569d2b06053b4728e6b
machrait <achrait.meryem@gmail.com>
2020-05-17 16:04:45 +0000

    438. Working with Refs & useRef()

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -7,28 +7,35 @@ const Search = React.memo(props =>
 {
 	const { onLoadIngredients } = props;
 	const [enteredFilter, setEnteredFilter] = useState('');
+	const inputRef = useRef();
 
 	useEffect(() =>
 	{
-		const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
-		fetch('https://reacthooks-9a40e.firebaseio.com/ingredients.json' + query).then(response =>
+		setTimeout(() =>
 		{
-			return response.json();
-		}).then(responseData =>
-		{	
-			const loadedIngredient = [];
-			for (const key in responseData)
+			if(enteredFilter === inputRef.current.value)
 			{
-				loadedIngredient.push(
-				{ 
-					id: key, 
-					title: responseData[key].title, 
-					amount: responseData[key].amount
+				const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`;
+				fetch('https://reacthooks-9a40e.firebaseio.com/ingredients.json' + query).then(response =>
+				{
+					return response.json();
+				}).then(responseData =>
+				{	
+					const loadedIngredient = [];
+					for (const key in responseData)
+					{
+						loadedIngredient.push(
+						{ 
+							id: key, 
+							title: responseData[key].title, 
+							amount: responseData[key].amount
+						});
+					}
+					onLoadIngredients(loadedIngredient);
 				});
 			}
-			onLoadIngredients(loadedIngredient);
-		});
-	}, [enteredFilter, onLoadIngredients]);
+		}, 500)
+	}, [enteredFilter, onLoadIngredients, inputRef]);
 
 
 	return (
@@ -37,6 +44,7 @@ const Search = React.memo(props =>
 				<div className="search-input">
 					<label>Filter by Title</label>
 					<input 
+						ref = {inputRef} 
 						type="text" 
 						value={enteredFilter} 
 						onChange={event => setEnteredFilter(event.target.value)}
